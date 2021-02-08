354938120568132a85124f2e5e09cbbd81d3a5fb
Gautier Maud <maud.j.gautier@gmail.com>
2021-02-08 18:05:01 +0100

    Config file for aRMS

--- config/TSS_to_ChIP_peak_distance_for_aRMS.sh
+++ config/TSS_to_ChIP_peak_distance_for_aRMS.sh
@@ -2,7 +2,7 @@
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 #                                                                              #
-# TSS_to_ChIP_peak_distance_for_DSRCT.sh                                       #
+# TSS_to_ChIP_peak_distance_for_aRMS.sh                                        #
 #                                                                              #
 # By: Maud Gautier <https://github.com/MaudGautier>, 2021                      #
 #                                                                              #
@@ -11,7 +11,7 @@
 #                                                                              #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-# Config plot to get the TSS to ChIP peak distances for DSRCT neotranscripts.
+# Config plot to get the TSS to ChIP peak distances for aRMS neotranscripts.
 
 
 
@@ -24,14 +24,14 @@
 input_dir=/data/kdi_prod/project_result/726/27.01/results/1_Data/
 chrom_sizes_hg19=/data/annotations/pipelines/Human/hg19/genome/chrom_hg19.sizes
 chrom_sizes_sorted=${input_dir}/chrom_hg19.sizes.sorted
-ChIP_peaks_file=/data/kdi_prod/project_result/726/27.03/results/4_Macs/ChIP-EWS-WT1_ChIP-EWS-WT1-INPUT_0.05_peaks_NO_BLACKLIST.narrowPeak
-gtf_file=/data/kdi_prod/project_result/726/27.03/results/1_Data/200624_DSRCT_neos_NEW_IDs.gtf
+ChIP_peaks_file=/data/kdi_prod/project_result/726/27.02/results/4_Macs/ChIP-PF_ChIP-PF_INPUT_0.05_peaks.narrowPeak
+gtf_file=/data/kdi_prod/project_result/726/27.02/results/1_Data/200624_aRMS_neos_NEW_IDs.gtf
 hg19_gtf_file=/data/annotations/pipelines/Human/hg19/gtf/gencode.v19.annotation.sorted.gtf
 
 # Output
 output_dir=/data/kdi_prod/project_result/726/27.01/results/tests_distances
 temp_dir=${output_dir}/temp
-ChIP_summits=${output_dir}/EwS_WT1_ChIP_peaks_summits.txt
+ChIP_summits=${output_dir}/PF_ChIP_peaks_summits.txt
 
 
 
@@ -58,8 +58,8 @@ awk -v OFS="\t" '{print $1, $2+$10, $4}' ${ChIP_peaks_file} \
 #             Get TSS to ChIP peak distance for EwS neotranscripts             #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-# Get TSS to EwS_WT1 ChIP peak distance for EwS neotranscripts
-output_file=${output_dir}/Closest_TSS_from_EwS_WT1_ChIP_peak_for_EwS_neotranscripts.txt
+# Get TSS to PF ChIP peak distance for EwS neotranscripts
+output_file=${output_dir}/Closest_TSS_from_PF_ChIP_peak_for_EwS_neotranscripts.txt
 sh ./src/sh/TSS_to_ChIP_peak_distance.sh \
 	-i $gtf_file \
 	-o $output_file \
@@ -67,8 +67,8 @@ sh ./src/sh/TSS_to_ChIP_peak_distance.sh \
 	-s $ChIP_summits
 
 
-# Get TSS to EwS_WT1 ChIP peak distance for all hg19 transcrips
-output_file=${output_dir}/Closest_TSS_from_EwS_WT1_ChIP_peak_for_all_hg19_transcripts.txt
+# Get TSS to PF ChIP peak distance for all hg19 transcrips
+output_file=${output_dir}/Closest_TSS_from_PF_ChIP_peak_for_all_hg19_transcripts.txt
 sh ./src/sh/TSS_to_ChIP_peak_distance.sh \
 	-i $hg19_gtf_file \
 	-o $output_file \
